35203aeecdf929429e180ab013d7fba0a22f77f9
Olya Tatianina <olya.tatianina@gmail.com>
2022-10-09 20:32:27 +0400

    code updated once more

--- main.py
+++ main.py
@@ -44,45 +44,41 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 get_shop_list_by_dishes(['Фахитос', 'Омлет'], 5)
 
-with open('1.txt', 'rt') as file:
+my_dic = {}
+with open('1.txt', 'rt', encoding='utf-8') as file:
     file_1_list = file.readlines()
     file_1_len = (len(file_1_list))
     file_1 = ' '.join(file_1_list)
+    my_dic[file_1_len] = ['1.txt', file_1]
 
-
-with open('2.txt', 'rt') as file:
+with open('2.txt', 'rt', encoding='utf-8') as file:
     file_2_list = file.readlines()
     file_2_len = (len(file_2_list))
     file_2 = ' '.join(file_2_list)
+    my_dic[file_2_len] = ['2.txt', file_2]
 
 
-with open('3.txt', 'rt') as file:
+with open('3.txt', 'rt', encoding='utf-8') as file:
     file_3_list = file.readlines()
     file_3_len = (len(file_3_list))
     file_3 = ' '.join(file_3_list)
+    my_dic[file_3_len] = ['3.txt', file_3]
 
-
+my_dic = dict(sorted(my_dic.items()))
 my_list = []
-dic_common = {
-    file_1_len: ['1.txt', file_1],
-    file_2_len: ['2.txt', file_2],
-    file_3_len: ['3.txt', file_3]
-    }
-
-dic_common = dict(sorted(dic_common.items()))
 
 
-with open('res.txt', 'w') as file:
-    for key in dic_common.keys():
+with open('res.txt', 'w', encoding='utf-8') as file:
+    for key in my_dic.keys():
         my_list.append(key)
     my_list = sorted(my_list)
-    for key in dic_common.keys():
+    for key in my_dic.keys():
         if key == min(my_list):
-            file.writelines(f'{dic_common[key][0]}\n{key}\n'
-                            f'{dic_common[key][1]}')
+            file.writelines(f'{my_dic[key][0]}\n{key}\n'
+                            f'{my_dic[key][1]}')
         elif key == max(my_list):
-            file.writelines(f'{dic_common[key][0]}\n{key}\n'
-                            f'{dic_common[key][1]}')
+            file.writelines(f'{my_dic[key][0]}\n{key}\n'
+                            f'{my_dic[key][1]}')
         else:
-            file.writelines(f'{dic_common[key][0]}\n{key}\n'
-                            f'{dic_common[key][1]}')
+            file.writelines(f'{my_dic[key][0]}\n{key}\n'
+                            f'{my_dic[key][1]}')
